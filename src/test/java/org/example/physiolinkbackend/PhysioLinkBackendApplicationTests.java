ff742a99da3ea7578b76842eff825b5c0bb217fc
John Rueda <johnrueda7@hotmail.com>
2024-02-25 23:08:57 -0500

    Se crea el proyecto y configuracion basica, se debe implementar Swagger y añadir info al readme

---
+++ src/test/java/org/example/physiolinkbackend/PhysioLinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.physiolinkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysioLinkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
